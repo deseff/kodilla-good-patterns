80e22992dad51b4498ae1c1ecab569d00b2eda54
deseff <blazej.kurjata@gmail.com>
2019-10-28 23:16:48 +0100

    Module 13.2 - solution of task - improvements (System.out.println)

--- src/main/java/com/kodilla/good/patterns/challenges/CDOrderRepository.java
+++ src/main/java/com/kodilla/good/patterns/challenges/CDOrderRepository.java
@@ -6,6 +6,8 @@ public class CDOrderRepository implements OrderRepository {
 
     @Override
     public boolean createOrder(User user, LocalDateTime orderDate) {
+        System.out.println("Order for user " + user.getNickName() + " " + user.getFirstName()  + " " + user.getLastName() +
+                " has been created on" + " " + orderDate);
         return true;
     }
 }
--- src/main/java/com/kodilla/good/patterns/challenges/CDOrderService.java
+++ src/main/java/com/kodilla/good/patterns/challenges/CDOrderService.java
@@ -4,8 +4,15 @@ import java.time.LocalDateTime;
 
 public class CDOrderService implements OrderService {
 
+    OrderRequestRetriever orderRequestRetriever = new OrderRequestRetriever();
+    OrderRequest orderRequest = orderRequestRetriever.retrieve();
+
     @Override
     public boolean order(User user, LocalDateTime orderDate, Product product, Double quantity) {
+        System.out.println("User " + user.getNickName() + " " + user.getFirstName() + " " + user.getLastName() +
+                " created order for " + orderRequest.getQuantity() + " piece(s) of "  + orderRequest.getProduct().getProductName() +
+                " on " + orderRequest.getOrderDate() +"." + "\n" + "Total amount to be paid equals " +
+                orderRequest.getQuantity() * orderRequest.getProduct().getPrice() + " PLN.");
         return true;
     }
 }
